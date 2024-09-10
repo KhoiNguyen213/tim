f936593c9e4f07f157e8b86c90f5d1b330a27bf1
KhoiNguyen213 <khoinguyendang151209@gmail.com>
2024-09-10 23:04:31 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,15 @@
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+
+one.onmouseenter = function () {
+  one.innerHTML = "Yes";
+};
+one.onmouseleave = function () {
+  one.innerHTML = "No";
+};
+one.onclick = function () {
+  alert("Thanks");
+};
+two.onclick = function () {
+  alert("real??");
+};
